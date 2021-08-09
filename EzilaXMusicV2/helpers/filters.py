410468a271945ae1832c1cd40c1feac1846837aa
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-08 22:46:42 -0700

    Update filters.py

--- EzilaXMusicV2/helpers/filters.py
+++ EzilaXMusicV2/helpers/filters.py
@@ -19,7 +19,7 @@ from typing import List, Union
 
 from pyrogram import filters
 
-from EzilaXMusic2.O.config import COMMAND_PREFIXES
+from EzilaXMusicV2.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
